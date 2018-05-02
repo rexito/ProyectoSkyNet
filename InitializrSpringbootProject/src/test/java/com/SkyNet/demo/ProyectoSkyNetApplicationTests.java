e9ab02a1bc5770eaccad69415eb812291ddbbe20
rexit <rexit@rexit-PC>
2018-05-02 18:21:04 -0300

    Primera Prueba

---
+++ InitializrSpringbootProject/src/test/java/com/SkyNet/demo/ProyectoSkyNetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.SkyNet.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectoSkyNetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
